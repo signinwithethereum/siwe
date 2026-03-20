b7417032025836e2c8c4aeacbbc1c13ec1530d6f
jalil <jalil@1001.digital>
2026-03-20 09:31:59 +0100

    Fix regex

--- packages/siwe-parser/lib/utils.ts
+++ packages/siwe-parser/lib/utils.ts
@@ -32,7 +32,7 @@ export const parseIntegerNumber = (number: string): number => {
 };
 
 const ISO8601 =
-  /^(?<date>[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]))[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/;
+  /^(?<date>[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]))[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([+-]([01][0-9]|2[0-3]):[0-5][0-9]))$/;
 
 export const isValidISO8601Date = (inputDate: string): boolean => {
   const inputMatch = ISO8601.exec(inputDate);
@@ -41,8 +41,7 @@ export const isValidISO8601Date = (inputDate: string): boolean => {
     return false;
   }
 
-  const inputDateParsed = new Date(inputMatch.groups.date).toISOString();
-  const parsedInputMatch = ISO8601.exec(inputDateParsed);
+  const roundTripped = new Date(inputMatch.groups.date).toISOString().slice(0, 10);
 
-  return inputMatch.groups.date === parsedInputMatch.groups.date;
+  return inputMatch.groups.date === roundTripped;
 };
