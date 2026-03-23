2c0caa987021e8ec192c06ea5fb46486599cb8cd
jalil <jalil@1001.digital>
2026-03-23 09:38:07 +0100

    Update vite config

--- packages/siwe-parser/vite.config.ts
+++ packages/siwe-parser/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/parsers.ts'),
+      entry: 'lib/parsers.ts',
       formats: ['es', 'cjs'],
       fileName: (format) => `parsers.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
--- packages/siwe/vite.config.ts
+++ packages/siwe/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/siwe.ts'),
+      entry: 'lib/siwe.ts',
       formats: ['es', 'cjs'],
       fileName: (format) => `siwe.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
